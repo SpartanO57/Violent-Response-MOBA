8e7a9f431b2797fd212a13ace3c7b3cd149019cb
Enrique E Perez II <SpartanO57@yahoo.com>
2018-04-27 02:52:37 -0500

    Complete overhaul
    Had alot of problems that were found in testing of jQuery for the test system. The .js was misnamed, and document was not put on the "getElementById". Added version number comment to bottom of page.
    Ver. 0.02

--- ASB.js
+++ ASB.js
@@ -1,11 +1,11 @@
-var timer = getElementById('timer');
+var timer = document.getElementById('timer');
 
-var armCode = getElementById('arm');
+var armCode = document.getElementById('arm');
 
-var disarmCode = getElementById('disarm');
+var disarmCode = document.getElementById('disarm');
 
 function setTimer() {
-  var timer = getElementById('timer');
+  var timer = document.getElementById('timer');
   if (timer < 1 || timer > 600) {
     window.alert("Timer must be set between 1 & 600 seconds");
   } else {
@@ -14,7 +14,7 @@ function setTimer() {
 };
 
 function setArmCode() {
-  var armCode2 = getElementById('arm');
+  var armCode2 = document.getElementById('arm');
   if (armCode2 == armCode) {
     window.alert("Ready To Arm");
   } else {
@@ -23,10 +23,18 @@ function setArmCode() {
 };
 
 function setDisarmCode() {
-  var disarmCode2 = getElementById('disarm');
+  var disarmCode2 = document.getElementById('disarm');
   if (disarmCode2 == disarmCode) {
     window.alert("Disarmed!");
   } else {
     window.alert("BOOM YOU'RE DEAD!!!");
   }
 };
+
+console.log(timer);
+console.log(armCode);
+console.log(armCode2);
+console.log(disarmCode);
+console.log(disarmCode2);
+
+//Ver. 0
