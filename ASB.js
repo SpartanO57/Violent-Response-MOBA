93eae34647ee8cd02cb5c3386fae55552be01696
Enrique Perez II <SpartanO57@yahoo.com>
2018-07-14 04:41:13 -0500

    Ver. 0.05
    stability updates

--- ASB.js
+++ ASB.js
@@ -1,8 +1,17 @@
+// Variable Area
+
+let timer = 0;
+
+let armCode = 1234;
+
+let armCode2 = 1234;
+
 /*This is the function to set the timer for the bomb. I hope to eventually add
 a fully working timer, but for now i'm concentrating on just getting the basic
 time frame settings and having it work when the buttons are pressed.*/
 
 function setTimer() {
+  timer = document.getElementById('timer');
   if (timer > 1 && timer < 600) {
     window.alert("Timer Set!");
   } else {
@@ -11,6 +20,8 @@ function setTimer() {
 };
 
 function setArmCode() {
+  armCode = document.getElementById('armCode');
+  armCode2 = document.getElementById('armCode2');
   if (armCode2 == armCode) {
     window.alert("Ready To Arm");
   } else {
@@ -19,6 +30,9 @@ function setArmCode() {
 };
 
 function setDisarmCode() {
+  disarmCode = document.getElementById('disarmCode');
+  disarmCode2 = document.getElementById('disarmCode2');
+  console.log(disarmCode + disarmCode2);
   if (disarmCode == disarmCode2) {
     window.alert("Disarmed!");
   } else {
@@ -27,14 +41,3 @@ function setDisarmCode() {
 };
 
 //Ver. 0.05
-
-
-//Test Area
-
-
-let armCode = document.getElementById('armCode');
-
-let armCode2 = document.getElementById('armCode2');
-let disarmCode = document.getElementById('disarmCode');
-let disarmCode2 = document.getElementById('disarmCode2');
-let timer = document.getElementById('timer');
